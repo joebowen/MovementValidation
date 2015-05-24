6d7d1f6e9017701e7ccb06635684c36ff38113b4
Joe Bowen <joe.bowen@toptal.com>
2015-05-24 10:58:13 -0600

    Starting work on main app.

--- wsgi/Openworm_Project/Openworm_Project/wsgi.py
+++ wsgi/Openworm_Project/Openworm_Project/wsgi.py
@@ -6,9 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
+'''
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Openworm_Project.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
+'''
+def application(environ, start_response):
+    if environ['mod_wsgi.process_group'] != '':
+        import signal
+        os.kill(os.getpid(), signal.SIGINT)
+    return ["killed"]
